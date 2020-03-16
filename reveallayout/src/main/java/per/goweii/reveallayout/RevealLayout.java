387449f4794b810f1696196d38e35d97fb6e8987
goweii <goweii@163.com>
2020-03-16 23:14:36 +0800

    支持代码切换时自定义中心的，支持隐藏背后View，支持监听点击事件

--- reveallayout/src/main/java/per/goweii/reveallayout/RevealLayout.java
+++ reveallayout/src/main/java/per/goweii/reveallayout/RevealLayout.java
@@ -9,7 +9,6 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.MotionEvent;
